1224d956fb722d06ae32539f66592b55c3c754a9
ural <uralbaxtiyorov8@gmail.com>
2024-09-05 21:36:20 +0500

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.querySelector('.modal').classList.add('modal-active')
+}
+
+function closeModal() {
+    document.querySelector('.modal').classList.remove('modal-active')
+}
